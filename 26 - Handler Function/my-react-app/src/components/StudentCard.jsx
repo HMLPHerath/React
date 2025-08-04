7f2fed19e99e46e37b02d8d866f0e76d6e4f7c40
HMLPHerath <156527514+HMLPHerath@users.noreply.github.com>
2025-08-04 13:35:30 +0530

    Handle function

---
+++ 26 - Handler Function/my-react-app/src/components/StudentCard.jsx
@@ -0,0 +1,17 @@
+import Student from "./Student";
+import React from "react";
+
+const StudentCard = ({ name, age, homeTown }) => {
+  return (
+    <div className="st-card">
+      <img
+        src="https://cdn.pixabay.com/photo/2019/05/21/22/21/boy-4220282_1280.png"
+        alt={`Student Image_${name}`}
+        className="student-image"
+      />
+      <Student name={name} age={age} homeTown={homeTown} />
+    </div>
+  );
+};
+
+export default StudentCard;
